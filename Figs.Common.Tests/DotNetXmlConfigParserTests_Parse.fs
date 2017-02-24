1d15c16898730026059e6d9d2e27f3fd8955a750
Filipe Silva <f.silva@coolblue.nl>
2017-02-24 08:02:24 +0100

    Add tests project with parser tests

---
+++ Figs.Common.Tests/DotNetXmlConfigParserTests_Parse.fs
@@ -0,0 +1,140 @@
+﻿module Figs.Common.Tests.DotNetXmlConfigParserTests.Parse
+
+open Xunit
+open FsUnit.Xunit
+
+open Figs.Common
+
+let CONNECTION_STRING_PREFIX = DotNetXmlConfigParser.ConnectionStrings.CONNECTION_STRING_PREFIX
+
+[<Fact>]
+let ``parse with empty string returns an empty list``() =
+    let input = ""
+    let output = DotNetXmlConfigParser.parse input
+
+    output |> should be Empty
+
+[<Fact>]
+let ``parse with invalid XML returns an empty list``() =
+    let input = "something"
+
+    DotNetXmlConfigParser.parse input |> should be Empty
+
+[<Fact>]
+let ``parse with valid XML but no appSettings / connectionStrings sections returns empty list``() =
+    let input = @"<configuration>
+                    <someOtherSection>
+                    </someOtherSection>
+                  </configuration>"
+
+    DotNetXmlConfigParser.parse input |> should be Empty
+
+[<Fact>]
+let ``parse with valid XML but empty appSettings section returns empty list``() =
+    let input = @"<configuration>
+                    <appSettings>
+                    </appSettings>
+                  </configuration>"
+
+    DotNetXmlConfigParser.parse input |> should be Empty
+
+[<Fact>]
+let ``parse with valid XML but empty connectionStrings section returns empty list``() =
+    let input = @"<configuration>
+                    <connectionStrings>
+                    </connectionStrings>
+                  </configuration>"
+
+    DotNetXmlConfigParser.parse input |> should be Empty
+
+[<Fact>]
+let ``parse with valid XML but empty appSettings and connectionStrings section returns empty list``() =
+    let input = @"<configuration>
+                    <appSettings>
+                    </appSettings>
+                    <connectionStrings>
+                    </connectionStrings>
+                  </configuration>"
+
+    DotNetXmlConfigParser.parse input |> should be Empty
+
+[<Fact>]
+let ``parse with valid XML and only appSettings section returns list with appropriate setting``() =
+    let input = @"<configuration>
+                    <appSettings>
+                        <add key=""TestKey"" value=""TestValue"" />
+                    </appSettings>
+                  </configuration>"
+
+
+    let expectedResult = [("TestKey", "TestValue")]
+    DotNetXmlConfigParser.parse input |> should equal expectedResult
+
+[<Fact>]
+let ``parse with valid XML and only connectionStrings section returns list with appropriate setting``() =
+    let input = @"<configuration>
+                    <connectionStrings>
+                        <add name=""TestName"" connectionString=""TestConnectionString"" />
+                    </connectionStrings>
+                  </configuration>"
+
+
+    let expectedName = CONNECTION_STRING_PREFIX + "TestName"
+    let expectedResult = [(expectedName, "TestConnectionString")]
+    DotNetXmlConfigParser.parse input |> should equal expectedResult
+
+[<Fact>]
+let ``parse with valid XML and both appSettings and connectionStrings sections with a single setting each returns list with all settings``() =
+    let input = @"<configuration>
+                    <connectionStrings>
+                        <add name=""TestName"" connectionString=""TestConnectionString"" />
+                    </connectionStrings>
+                    <appSettings>
+                        <add key=""TestKey"" value=""TestValue"" />
+                    </appSettings>
+                  </configuration>"
+
+
+    let expectedConnectionStringName = CONNECTION_STRING_PREFIX + "TestName"
+    let expectedConnectionStringResult = (expectedConnectionStringName, "TestConnectionString")
+
+    let expectedAppSettingsResult = ("TestKey", "TestValue")
+
+    let expectedResult = [expectedAppSettingsResult; expectedConnectionStringResult]
+
+    DotNetXmlConfigParser.parse input |> should equal expectedResult
+
+
+
+let listsAreEqual list1 list2 =  set list1 = set list2
+
+[<Fact>]
+let ``parse with valid XML and both appSettings and connectionStrings sections with multiple setting each returns list with all settings``() =
+    let input = @"<configuration>
+                    <connectionStrings>
+                        <add name=""TestName1"" connectionString=""TestConnectionString1"" />
+                        <add name=""TestName2"" connectionString=""TestConnectionString2"" />
+                        <add name=""TestName3"" connectionString=""TestConnectionString3"" />
+                    </connectionStrings>
+                    <appSettings>
+                        <add key=""TestKey1"" value=""TestValue1"" />
+                        <add key=""TestKey2"" value=""TestValue2"" />
+                    </appSettings>
+                  </configuration>"
+
+
+    let expectedConnectionStringName1 = CONNECTION_STRING_PREFIX + "TestName1"
+    let expectedConnectionStringName2 = CONNECTION_STRING_PREFIX + "TestName2"
+    let expectedConnectionStringName3 = CONNECTION_STRING_PREFIX + "TestName3"
+    let expectedConnectionStringResult1 = (expectedConnectionStringName1, "TestConnectionString1")
+    let expectedConnectionStringResult2 = (expectedConnectionStringName2, "TestConnectionString2")
+    let expectedConnectionStringResult3 = (expectedConnectionStringName3, "TestConnectionString3")
+
+    let expectedAppSettingsResult1 = ("TestKey1", "TestValue1")
+    let expectedAppSettingsResult2 = ("TestKey2", "TestValue2")
+
+    let expectedResult = [expectedAppSettingsResult1; expectedAppSettingsResult2; expectedConnectionStringResult1; expectedConnectionStringResult2; expectedConnectionStringResult3]
+    let result = DotNetXmlConfigParser.parse input
+
+    listsAreEqual result expectedResult |> should equal true
+
---
+++ Figs.Common.Tests/DotNetXmlConfigParserTests_Write.fs
@@ -0,0 +1,115 @@
+﻿module Figs.Common.Tests.DotNetXmlConfigParserTests.Write
+
+open Xunit
+open FsUnit.Xunit
+open System.Xml.Linq
+
+open Figs.Common
+
+let makeConnectionStringName = DotNetXmlConfigParser.ConnectionStrings.makeConnectionStringName
+
+[<Fact>]
+let ``write with empty list returns empty but valid xml string``() =
+    let input = []
+    let expectedXml = new XElement(XName.Get("configuration"))
+
+    let output = DotNetXmlConfigParser.write input
+    let xmlOutput = XElement.Parse(output)
+
+    xmlOutput.ToString() |> should equal (expectedXml.ToString())
+
+[<Fact>]
+let ``write with single app setting returns xml string with correct appSettings section only``() =
+    let input = [("TestKey", "TestValue")]
+    let expectedOutput = @"<configuration>
+                                <appSettings>
+                                    <add key=""TestKey"" value=""TestValue"" />
+                                </appSettings>
+                           </configuration>"
+    let expectedXml = XElement.Parse(expectedOutput)
+
+    let output = DotNetXmlConfigParser.write input
+    let xmlOutput = XElement.Parse(output)
+
+    xmlOutput.ToString() |> should equal (expectedXml.ToString())
+
+[<Fact>]
+let ``write with single connection string returns xml string with correct connectionStrings section only``() =
+    let input = [(makeConnectionStringName("TestName"), "TestConnectionString")]
+    let expectedOutput = @"<configuration>
+                                <connectionStrings>
+                                    <add name=""TestName"" connectionString=""TestConnectionString"" />
+                                </connectionStrings>
+                           </configuration>"
+    let expectedXml = XElement.Parse(expectedOutput)
+
+    let output = DotNetXmlConfigParser.write input
+    let xmlOutput = XElement.Parse(output)
+
+    xmlOutput.ToString() |> should equal (expectedXml.ToString())
+
+[<Fact>]
+let ``write with multiple app settings returns xml string with correct appSettings only, in order``() =
+    let input = [("TestKey1", "TestValue1");
+                 ("TestKey2", "TestValue2");
+                 ("TestKey3", "TestValue3")]
+    let expectedOutput = @"<configuration>
+                                <appSettings>
+                                    <add key=""TestKey1"" value=""TestValue1"" />
+                                    <add key=""TestKey2"" value=""TestValue2"" />
+                                    <add key=""TestKey3"" value=""TestValue3"" />
+                                </appSettings>
+                           </configuration>"
+    let expectedXml = XElement.Parse(expectedOutput)
+
+    let output = DotNetXmlConfigParser.write input
+    let xmlOutput = XElement.Parse(output)
+
+    xmlOutput.ToString() |> should equal (expectedXml.ToString())
+
+[<Fact>]
+let ``write with multiple connection strings returns xml string with correct connectionStrings only, in order``() =
+    let input = [(makeConnectionStringName("TestName1"), "TestConnectionString1");
+                 (makeConnectionStringName("TestName2"), "TestConnectionString2");
+                 (makeConnectionStringName("TestName3"), "TestConnectionString3")]
+    let expectedOutput = @"<configuration>
+                                <connectionStrings>
+                                    <add name=""TestName1"" connectionString=""TestConnectionString1"" />
+                                    <add name=""TestName2"" connectionString=""TestConnectionString2"" />
+                                    <add name=""TestName3"" connectionString=""TestConnectionString3"" />
+                                </connectionStrings>
+                           </configuration>"
+    let expectedXml = XElement.Parse(expectedOutput)
+
+    let output = DotNetXmlConfigParser.write input
+    let xmlOutput = XElement.Parse(output)
+
+    xmlOutput.ToString() |> should equal (expectedXml.ToString())
+
+[<Fact>]
+let ``write with multiple app settings and connection strings returns correct xml string with all settings in order``() =
+    let input = [(makeConnectionStringName("TestName1"), "TestConnectionString1");
+                 ("TestKey1", "TestValue1");
+                 ("TestKey2", "TestValue2");
+                 (makeConnectionStringName("TestName2"), "TestConnectionString2");
+                 (makeConnectionStringName("TestName3"), "TestConnectionString3");
+                 ("TestKey3", "TestValue3")]
+
+    let expectedOutput = @"<configuration>
+                                <appSettings>
+                                    <add key=""TestKey1"" value=""TestValue1"" />
+                                    <add key=""TestKey2"" value=""TestValue2"" />
+                                    <add key=""TestKey3"" value=""TestValue3"" />
+                                </appSettings>
+                                <connectionStrings>
+                                    <add name=""TestName1"" connectionString=""TestConnectionString1"" />
+                                    <add name=""TestName2"" connectionString=""TestConnectionString2"" />
+                                    <add name=""TestName3"" connectionString=""TestConnectionString3"" />
+                                </connectionStrings>
+                           </configuration>"
+    let expectedXml = XElement.Parse(expectedOutput)
+
+    let output = DotNetXmlConfigParser.write input
+    let xmlOutput = XElement.Parse(output)
+
+    xmlOutput.ToString() |> should equal (expectedXml.ToString())
