82af095729c969570ea17594cf41d9d0a55db2b3
Filipe Silva <f.silva@coolblue.nl>
2017-02-28 23:50:12 +0100

    Add Common.Data module with data and repository definitions

---
+++ Figs.Common/Data.fs
@@ -0,0 +1,29 @@
+﻿module Figs.Common.Data
+
+type public ConfigurationType = DotNetXml = 0
+
+type public Configuration(name, url, configType : ConfigurationType, values : Map<string, string>) =
+    member this.Name = name
+    member this.Url = url
+    member this.Type = configType
+    member this.Values = values
+
+type public IConfigurationRepository =
+    abstract member Get: string -> Configuration
+    abstract member Save: Configuration -> unit
+    abstract member Delete: string -> unit
+
+type fetchUrlContents = string -> string
+
+
+type public DummyRepository() =
+    interface IConfigurationRepository with
+        member this.Get name =
+            new Configuration(name, "", ConfigurationType.DotNetXml, Map.empty<string, string>)
+
+        member this.Save config =
+            "" |> ignore
+
+        member this.Delete config =
+            "" |> ignore
+
